481b642b84ce0f63632432619e097f2d8569f26b
megabitek <megabitek@yandex.ru>
2015-06-10 11:56:31 +0300

    вывела на экран frame

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package snake;
-
+import javax.swing.*;
 /**
  *
  * @author admin
@@ -16,6 +16,10 @@ public class Snake {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JFrame mainFrame = new JFrame("Java Snake");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(1100, 600);
+        mainFrame.setVisible(true);
     }
     
 }
